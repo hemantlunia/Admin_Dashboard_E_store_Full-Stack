e0f118980b15c22d84357e34e1204db7ab87f4bd
Hemant Kumar <your.email@example.com>
2025-04-18 18:47:14 +0530

    add

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ if (process.env.NODE_ENV==="production") {
     app.use(express.static(path.join(__dirname,"../admin/dist")));
 
 
-    app.get("*",(req,res)=>{
+    app.get("/*splat",(req,res)=>{
         res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
     })
 }
